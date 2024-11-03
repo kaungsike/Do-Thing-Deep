eb8f0a39a475e7317601a9665482fde5c01c31d1
kaungsike <kaungsike9@gmail.com>
2024-11-03 15:44:00 +0630

    learning

--- go/03userInput/main.go
+++ go/03userInput/main.go
@@ -1,8 +1,19 @@
-package mian
+package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	welcome := "Welcome to user input"
 	fmt.Println(welcome)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating for our pizza : ")
+
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thnak for reading, ",input)
+	fmt.Printf("Type of this rating is %T, ",input)
 }
