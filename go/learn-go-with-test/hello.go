c7b07fce46211c3b02b85d84f03b163f172b164f
kaungsike <kaungsike9@gmail.com>
2024-11-09 10:30:11 +0630

    learn go

--- go/learn-go-with-test/hello.go
+++ go/learn-go-with-test/hello.go
@@ -1,13 +1,26 @@
 package main
 
 import "fmt"
+const spanish = "Spanish"
+const myanmar = "Myanmar"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const myanmarhHelloPrefix = "Minkalrbr, "
 
-// import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+func Hello(name,language string) string {
+	if name == "" {
+		name = "World"
+	}
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+	if language == myanmar {
+		return myanmarhHelloPrefix + name
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world",""))
 }
--- go/learn-go-with-test/hello_test.go
+++ go/learn-go-with-test/hello_test.go
@@ -3,10 +3,37 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("syaing hello to people", func(t *testing.T) {
+		got := Hello("s","")
+		want := "Hello, s"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("","")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Myanmar", func(t *testing.T) {
+		got := Hello("Ks", "Myanmar")
+		want := "Minkalrbr, Ks"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("Got %q and want %q", got, want)
 	}
 }
