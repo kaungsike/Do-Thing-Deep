5bf5b79bee5ef7089c6e77b9ed84d816c6fb44be
kaungsike <kaungsike9@gmail.com>
2024-11-11 11:47:10 +0630

    golang

--- go/learn-go-with-test/array-and-slices/sum.go
+++ go/learn-go-with-test/array-and-slices/sum.go
@@ -1,12 +1,29 @@
 package main
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 
 	sum := 0
 
-	for _,number := range numbers {
+	for _, number := range numbers {
 		sum += number
 	}
 
 	return sum
+}
+
+// put array and it will return the sum of them
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, number := range numbersToSum {
+		if len(number) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tails := number[1:]
+			sums = append(sums, Sum(tails))
+		}
+	}
+
+	return sums
+
 }
--- go/learn-go-with-test/array-and-slices/sum_test.go
+++ go/learn-go-with-test/array-and-slices/sum_test.go
@@ -1,29 +1,52 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestSum(t *testing.T) {
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
 
-		sum := Sum(numbers)
-		want := 15
-
-		if sum != want {
-			t.Errorf("got %d but want %d, source %v", sum, want, numbers)
+	checkMessage := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
 		}
-	})
+	}
+
+	// t.Run("collection of 5 numbers", func(t *testing.T) {
+	// 	numbers := []int{1, 2, 3, 4, 5}
+
+	// 	got := Sum(numbers)
+	// 	want := 15
+
+	// 	checkMessage(t, got, want)
+	// })
 
 	// t.Run("collection of any size", func(t *testing.T) {
 	// 	numbers := []int{1, 2, 3}
 
-	// 	got := Sum([5]int(numbers))
+	// 	got := Sum([]int(numbers))
 	// 	want := 6
 
-	// 	if got != want {
-	// 		t.Errorf("got %d but want %d, source %v", got, want, numbers)
-	// 	}
+	// 	checkMessage(t, got, want)
 	// })
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+
+		want := []int{0, 9}
+
+		checkMessage(t, got, want)
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+	want := []int{2, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
