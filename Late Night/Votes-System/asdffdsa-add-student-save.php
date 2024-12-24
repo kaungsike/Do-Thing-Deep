4e5a63d765b4f9683c8c1b9ef07546f69bb43c76
kaungsike <kaungsike9@gmail.com>
2024-12-24 16:31:12 +0630

    re build voucher-app

---
+++ Late Night/Votes-System/asdffdsa-add-student-save.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once("./sql-connection.php");
+
+echo "<pre>";
+
+print_r($_POST);
+
+$name = $_POST['name'];
+$edu_mail = $_POST['edu_mail'];
+
+
+$sql = "select * from students where edu_mail='$edu_mail'";
+
+
+$query = mysqli_query($con,$sql);
+
+var_dump($query);
+
+
+if($query){
+    $data = mysqli_fetch_assoc($query);
+
+    if($data){
+        $add_student_sql = ""
+    }else{
+        "This edu mail already exis";
+    }
+
+    echo "Correct";
+}else{
+    echo "Smoething worng in sql";
+}
--- Late Night/Votes-System/asdffdsa-add-student.php
+++ Late Night/Votes-System/asdffdsa-add-student.php
@@ -2,7 +2,7 @@
 <?php require_once("./sql-connection.php") ?>
 
 <main class="container mx-auto py-8 px-4 min-h-screen">
-<?php include("./src/template/side-bar.php") ?>
+    <?php include("./src/template/side-bar.php") ?>
 
 
     <div class="w-full mb-5">
@@ -12,9 +12,21 @@
     </div>
 
     <div class="bg-white p-6 rounded-lg shadow">
-        <form action="">
-            
-        </form>
+        <h3 class="text-2xl font-bold mb-4">Add New Student</h3>
+        <hr class="mb-4 border-orange-500">
+        <div class="form-control w-full max-w-[1200px] p-[50px] mx-auto">
+            <form action="./asdffdsa-add-student-save.php" method="post" class="flex flex-col gap-10">
+                <div class="w-full flex items-center justify-between gap-10">
+                    <label for="" class="flex items-center w-[50%]">Name
+                        <input type="text" placeholder="" name="name" class="input ml-3 input-bordered flex-grow" />
+                    </label>
+                    <label class="flex items-center w-[50%]" for="">EDU mail
+                        <input type="email" placeholder="" name="edu_mail" class="input ml-3 input-bordered flex-grow" />
+                    </label>
+                </div>
+                <button class="border border-orange-500 text-white bg-orange-500 w-[450px] py-3 items-end ml-auto mt-5 rounded-lg">Add</button>
+            </form>
+        </div>
     </div>
 </main>
 
