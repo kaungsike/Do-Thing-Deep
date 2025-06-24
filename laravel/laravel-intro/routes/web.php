814f83bec6c3a9cf95079a483b5631bd2467d5c2
kaungsike <kaungsike9@gmail.com>
2025-06-24 23:43:36 +0630

    learn : laravel d 2

--- laravel/laravel-intro/routes/web.php
+++ laravel/laravel-intro/routes/web.php
@@ -2,6 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    // return view('welcome');
+    return "Home Page";
 });
+
+Route::get('/about', function () {
+    // return view('welcome');
+    return "About Page";
+});
+
+
+Route::get('/contact', function () {
+    return "Contact Page";
+});
+
+Route::get("/mac/{type}/{size}", function ($type, $size) {
+    return "Mac Page $type and size $size";
+});
+
+
+Route::get("/profile/{id?}", function ($id = null) {
+    return " User $id Profile Page";
+});
+
+Route::get(
+    'area/{width}/{height}',
+    fn($width, $height) => "Area of rectangle is $width * $height = " . ($width * $height)
+);
