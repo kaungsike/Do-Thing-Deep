1188b45fbe418e9e4e626f1611143a1bc00b0813
Kaung Sike <kaungsike9@gmail.com>
2026-05-13 17:52:09 +0630

    Add math module with addition and subtraction functions; implement main.js to log results

---
+++ nodejs/main.mjs
@@ -0,0 +1,4 @@
+import math from "./math-esm.mjs";
+
+console.log(math.add(3, 5))
+console.log(math.sub(3, 5))
---
+++ nodejs/math-esm.mjs
@@ -0,0 +1,10 @@
+//  export default (a, b) => {
+//     return a + b;
+// };
+
+// export default add;
+
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+
+export default{ add, sub };
