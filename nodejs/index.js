252629ebaf115fb54d017625d2474297dab06379
Kaung Sike <kaungsike9@gmail.com>
2026-05-13 16:41:25 +0630

    Add superhero modules and refactor index.js; implement IIFE for superhero logging

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,9 +1,3 @@
-const add = require('./add');
-
-console.log("Hello, World from Node.js!");
-
-const sum = add(5, 3);
-const sum2 = add(10, 20);
-
-console.log(sum, sum2);
+require('./batman');
+require('./superman');
 
