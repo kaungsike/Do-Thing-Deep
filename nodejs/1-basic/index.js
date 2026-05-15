13c98e2003b2458b58ba40446ddf6a4d9e6862bd
Kaung Sike <kaungsike9@gmail.com>
2026-05-15 17:28:08 +0630

    Comment out unused code and maintain structure for future reference in index.js

--- nodejs/1-basic/index.js
+++ nodejs/1-basic/index.js
@@ -8,10 +8,20 @@
 // console.log(add(2,4))
 // console.log(subtract(2,4))
 
-const {name} = require('./data.json');
+// const {name} = require('./data.json');
 
-console.log(name);
+// console.log(name);
 
-console.log("Hello Node");
+// console.log("Hello Node");
 
-console.log("first")
+// console.log("first")
+
+const crypto = require("node:crypto");
+
+const start = Date.now();
+crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+console.log("Hashing password took " + (Date.now() - start) + " milliseconds");
