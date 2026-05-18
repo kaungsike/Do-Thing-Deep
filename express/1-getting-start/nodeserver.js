c58c56fbe6a84bc2fe7a4b59fb2b4f481f3ff296
Kaung Sike <kaungsike9@gmail.com>
2026-05-18 17:14:56 +0630

    feat: implement basic Express server with HTML form for user input

--- express/1-getting-start/nodeserver.js
+++ express/1-getting-start/nodeserver.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-   res.send('Hello World');
+   res.sendFile(path.join(__dirname,"index.html"));
 })
 
-var server = app.listen(3000, function () {
-   console.log("Express App running at http://127.0.0.1:3000/");
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+var server = app.listen(5000, function () {
+   console.log("Express App running at http://127.0.0.1:5000/");
 })
